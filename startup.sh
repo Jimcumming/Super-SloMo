325ab7559f84b2069be17b92b8a17988d74ac8b3
Jim Cumming <jim@jcws.co.uk>
2019-12-09 14:15:31 +0000

    add checkpoint directory sync

--- startup.sh
+++ startup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 eval "$(conda shell.bash hook)"
 conda activate slomo
-aws s3 sync /checkpoints s3://slomo-checkpoints
+aws s3 cp s3://slomo-checkpoints/default.ckpt /home/ubuntu/checkpoints/default.ckpt
 cd /home/ubuntu/Super-SloMo
 git pull
 python script.py
